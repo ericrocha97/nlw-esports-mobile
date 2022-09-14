4c62c1097b24524467d3a4cceca8108d4294c1e3
Eric Rocha <ericrochaeric@gmail.com>
2022-09-14 09:33:08 -0300

    aula 02

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+import { ImageBackground, Text } from "react-native";
+
+import backgroundImg from "../../assets/background-galaxy.png";
+
+import { styles } from "./styles";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground
+      source={backgroundImg}
+      defaultSource={backgroundImg}
+      style={styles.container}
+    >
+      {children}
+    </ImageBackground>
+  );
+}
